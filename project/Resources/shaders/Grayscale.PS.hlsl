8704b05685c35bd4a6a1672024dd2f41f6c36125
LE2B_16 <k023g1064@g.neec.ac.jp>
2025-01-15 15:14:40 +0900

    完了

---
+++ project/Resources/shaders/Grayscale.PS.hlsl
@@ -0,0 +1,26 @@
+#include "Fullscreen.hlsli"
+
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    // テクスチャサンプリング
+    output.color = gTexture.Sample(gSampler, input.texcoord);
+
+    // グレースケール値の計算
+    float value = dot(output.color.rgb, float3(0.2125f, 0.7154f, 0.0721f));
+
+    // グレースケール値を適用
+    output.color.rgb = float3(value, value, value);
+    // セピア調(回想シーンなどで使える)
+    //output.color.rgb = value * float3(1.0f, 74.0f / 107.0f, 43.0f / 107.0f);
+
+    return output;
+}
