3747c21cc5c91de366154d9f989727aef47b32c3
LE2B_16 <k023g1064@g.neec.ac.jp>
2025-01-16 10:11:12 +0900

    ビネット完了

--- project/Resources/shaders/Vignette.PS.hlsl
+++ project/Resources/shaders/Vignette.PS.hlsl
@@ -16,8 +16,10 @@ PixelShaderOutput main(VertexShaderOutput input)
     // 周囲をθに、中心になるほど明るくなるように計算で調整
     float2 correct = input.texcoord * (1.0f - input.texcoord.yx);
     // correctだけで計算すると中心の最大値が0.0625で暗すぎるのでScaleで調整。この例では16倍して1にしている
+    // 16の値を小さくすればするほど暗い範囲が中心よりに狭くなる
     float vignette = correct.x * correct.y * 16.0f;
     // とりあえず0.8乗でそれっぽくしてみた
+    // 0.8を変えることで丸い形が濃くなったり薄くなったりする
     vignette = saturate(pow(vignette, 0.8f));
     // 係数として乗算
     output.color.rgb *= vignette;
