9e811f2c02e4e6aee667d8f34d2be226bc9e0b23
LE2B_16 <k023g1064@g.neec.ac.jp>
2025-03-14 16:43:40 +0900

    資料14ページまで

---
+++ project/Resources/shaders/LuminanceBasedOutLine.PS.hlsl
@@ -0,0 +1,77 @@
+#include "Fullscreen.hlsli"
+
+Texture2D gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+static const float2 kIndex3x3[3][3] =
+{
+    { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
+    { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
+    { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
+};
+
+static const float kKernel3x3[3][3] =
+{
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+};
+
+static const float kPrewittHorizontalKernel[3][3] =
+{
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f }
+};
+
+static const float kPrewittVerticalKernel[3][3] =
+{
+    { -1.0f / 6.0f, -1.0f / 6.0f, -1.0f / 6.0f },
+    { 0.0f, 0.0f, 0.0f },
+    { 1.0f / 6.0f, 1.0f / 6.0f, 1.0f / 6.0f }
+};
+
+float Luminance(float3 v)
+{
+    return dot(v, float3(0.2125f, 0.7154f, 0.0721f));
+}
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+     // 1. uvStepSizeの算出
+    uint width, height;
+    gTexture.GetDimensions(width, height);
+    float2 uvStepSize = float2(rcp(width), rcp(height));
+    
+    PixelShaderOutput output;
+    output.color.rgb = float3(0.0f, 0.0f, 0.0f);
+    output.color.a = 1.0f;
+    
+    float2 difference = float2(0.0f, 0.0f); // 縦横それぞれの畳み込みの結果を格納する
+    // 色を輝度に変換して、畳み込みを行なっていく。微分Filter用のkernelになっているので、やること自体は意アマまでの畳み込みと同じ
+    for (int x = 0; x < 3; ++x)
+    {
+        for (int y = 0; y < 3;++y)
+        {
+            float2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+            float luminance = Luminance(fetchColor);
+            difference.x += luminance * kPrewittHorizontalKernel[x][y];
+            difference.y += luminance * kPrewittVerticalKernel[x][y];
+            
+        }
+
+    }
+    
+    float weight = saturate(length(difference));
+    
+    output.color.rgb = weight;
+    output.color.a = 1.0f;
+    return output;
+}
+
